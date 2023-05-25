a7ace07948fd4bab03826b5df8f95086d924235f
Folaste <leblancflo@orange.fr>
2023-05-25 14:53:41 +0200

    Adding plugin.php

---
+++ plugin.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Tempete de cerveaux
+Plugin URI:  tempetedecerveaux.local
+Description: Plugin to add a chatbot in website
+Version: 0.1
+Author: Tempete de cerveaux
+Author URI: tempetedecerveaux.local
+*/
+
+echo 'Hello, World!'.PHP_EOL;
+?>
